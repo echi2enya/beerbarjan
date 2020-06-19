d0f4760c0e765f2176df141d594d35c9b7553e2d
Hiroyuki Echizenya <hiroyuki@echizenya.biz>
2020-06-19 11:22:30 +0900

    top swiper

--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,72 @@
 <template lang="pug">
   .l-content
     header.l-header
-      swiper
+      swiper(
+        :options = {
+          effect: 'fade',
+          speed: 1000,
+          loop: true,
+          autoplay: {
+            delay: 3000,
+            disableOnInteraction: false
+          }
+        }
+      )
         swiper-slide
-          .c-swiper
-            img.img(src='~assets/img/swiper/hieda.jpg')
+          img.c-swiper(src='~assets/img/swiper/hieda.jpg')
         swiper-slide
-          .c-swiper
-            img.img(src='~assets/img/swiper/counter.jpg')
+          img.c-swiper(src='~assets/img/swiper/counter.jpg')
         swiper-slide
-          .c-swiper
-            img.img(src='~assets/img/swiper/glass.jpg')
+          img.c-swiper(src='~assets/img/swiper/glass.jpg')
         swiper-slide
-          .c-swiper
-            img.img(src='~assets/img/swiper/sign.jpg')
+          img.c-swiper(src='~assets/img/swiper/sign.jpg')
         swiper-slide
-          .c-swiper
-            img.img(src='~assets/img/swiper/tap.jpg')
+          img.c-swiper(src='~assets/img/swiper/tap.jpg')
         swiper-slide
-          .c-swiper
-            img.img(src='~assets/img/swiper/blank.jpg')
+          img.c-swiper(src='~assets/img/swiper/blank.jpg')
         swiper-slide
-          .c-swiper
-            img.img(src='~assets/img/swiper/taps.jpg')
+          img.c-swiper(src='~assets/img/swiper/taps.jpg')
     main.inner
-      font-awesome-icon(icon='dollar-sign')
+      p 四ッ谷荒木町のビールバーJAN(ジャン)です
+      p 2018年8月1日オープン
+      p クラフトビール樽生4種、ヱビス樽生、特製の鶏のからあげ
+      p カウンター席、テーブル席、立ち飲みカウンター
+      p
+        | 日曜日定休
+        br
+        | ※月曜日が祝日の場合、土日営業し、月曜日がお休み
+      p クラフトビールは日々替わります！
+      p
+        | 新宿区荒木町2-15 2階
+        br
+        | 03-3226-1556
     footer
+      //- font-awesome-icon(icon='dollar-sign')
+      //- swiper
+        swiper-slide
+          img.c-swiper(src='~assets/img/swiper/hieda.jpg')
+        swiper-slide
+          img.c-swiper(src='~assets/img/swiper/counter.jpg')
+        swiper-slide
+          img.c-swiper(src='~assets/img/swiper/glass.jpg')
+        swiper-slide
+          img.c-swiper(src='~assets/img/swiper/sign.jpg')
+        swiper-slide
+          img.c-swiper(src='~assets/img/swiper/tap.jpg')
+        swiper-slide
+          img.c-swiper(src='~assets/img/swiper/blank.jpg')
+        swiper-slide
+          img.c-swiper(src='~assets/img/swiper/taps.jpg')
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
-import Logo from '~/components/Logo.vue'
+// import Logo from '~/components/Logo.vue'
 
 @Component({
-  components: {
-    Logo
-  }
+  // components: {
+  //   Logo
+  // }
 })
 
 export default class extends Vue {}
@@ -54,17 +85,19 @@ export default class extends Vue {}
 }
 .c-swiper {
   width: 100vw;
-  height: 100vh;
-  > .img {
-    object-fit: contain;
-    // object-position: center;
-  }
+  height: 90vh;
+  object-fit: cover;
 }
 @media (max-width: 767px) {
   .l-content {
     > .inner {
       width: 100%;
     }
+  }
+  .c-swiper {
+    height: calc(100vh / 7 * 5);
+    /* height: calc(100vw * 1.414); */
+    /* height: calc(100vw * 1.618); */
   }
 }
 </style>
