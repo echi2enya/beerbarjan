d95e646afa5b3c3315201c64e10ee2f7740bdd5e
Hiroyuki Echizenya <hiroyuki@echizenya.biz>
2020-06-27 12:17:38 +0900

    fontawesome brand icon

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,23 +1,39 @@
 <template lang="pug">
   footer.l-footer
     .c-footer
-      img.logo(src='~assets/img/logo/279x279.jpg')
-      nav.nav
-        nuxt-link.anchor(to='' v-scroll-to='"#top"') TOP
-        nuxt-link.anchor(to='' v-scroll-to='"#map"') MAP
-        nuxt-link.anchor(to='' v-scroll-to='"#information"') INFORMATION
-        nuxt-link.anchor(to='' v-scroll-to='"#news"') NEW
-        nuxt-link.anchor(to='' v-scroll-to='"#photo"') PHOTO
-      nav.sns
-        nuxt-link.anchor(to='https://twitter.com/beerbarjan')
-          font-awesome-icon(icon='twitter')
-        nuxt-link.anchor(to='https://www.facebook.com/pages/category/Product-Service/%E3%83%93%E3%83%BC%E3%83%AB%E3%83%90%E3%83%BC-JAN-1739676409488264/')
-          i.fas.fa-twitter
+      .link
+        nav.page
+          nuxt-link.anchor(to='' v-scroll-to='"#top"') TOP
+          nuxt-link.anchor(to='' v-scroll-to='"#map"') MAP
+          nuxt-link.anchor(to='' v-scroll-to='"#information"') INFORMATION
+          nuxt-link.anchor(to='' v-scroll-to='"#news"') NEW
+          nuxt-link.anchor(to='' v-scroll-to='"#photo"') PHOTO
+        //- nav.nav
+          nuxt-link.anchor(to='#top') TOP
+          nuxt-link.anchor(to='#map') MAP
+          nuxt-link.anchor(to='#informationn') INFORMATION
+          nuxt-link.anchor(to='#news') NEW
+          nuxt-link.anchor(to='#photo') PHOTO
+        nav.external
+          a.anchor(href='https://twitter.com/beerbarjan' target='_blank')
+            font-awesome-layers(class='fa-fw fa-2x')
+              font-awesome-icon(:icon='["fab", "twitter"]')
+          a.anchor(href='https://www.facebook.com/pages/category/Product-Service/%E3%83%93%E3%83%BC%E3%83%AB%E3%83%90%E3%83%BC-JAN-1739676409488264/' target='_blank')
+            font-awesome-layers(class='fa-fw fa-2x')
+              font-awesome-icon(:icon='["fab", "facebook"]')
+          a.anchor(href='https://www.google.com/maps/place/%E3%80%92160-0007+%E6%9D%B1%E4%BA%AC%E9%83%BD%E6%96%B0%E5%AE%BF%E5%8C%BA%E8%8D%92%E6%9C%A8%E7%94%BA+2%E9%9A%8E+%E5%89%8D%E7%94%B0%E3%83%93%E3%83%AB+%E3%83%93%E3%83%BC%E3%83%AB%E3%83%90%E3%83%BCJAN/@35.6887204,139.7224245,17z/data=!4m2!3m1!1s0x60188d5478ef5331:0x567a2eb3b90b0a8e' target='_blank')
+            font-awesome-layers(class='fa-fw fa-2x')
+              font-awesome-icon(:icon='["fab", "google"]')
+          a.anchor(href='https://tabelog.com/tokyo/A1309/A130903/13225631/' target='_blank')
+            font-awesome-layers(class='fa-fw fa-2x')
+              font-awesome-icon(:icon='["fas", "utensils"]')
       .information
-        p.arakicho 四ッ谷荒木町
-        p.name ビールバーJAN
-        p.address 新宿区荒木町2-15 2階
-        p.tel 03-3226-1556
+        img.logo(src='~assets/img/logo/279x279.jpg')
+        .text
+          p.arakicho 四ッ谷荒木町
+          h1.name ビールバーJAN
+          p.address 新宿区荒木町2-15 2階
+          p.tel 03-3226-1556
       //- p 2018年8月1日オープン
       //- p クラフトビール樽生4種、ヱビス樽生、特製の鶏のからあげ
       //- p カウンター席、テーブル席、立ち飲みカウンター
@@ -39,78 +55,123 @@
 .c-footer {
   max-width: 980px;
   height: 100px;
-  display: flex;
-  justify-content: flex-start;
-  align-items: flex-end;
+  // display: flex;
+  // justify-content: flex-start;
+  // align-items: flex-end;
+  // flex-wrap: wrap;
   position: relative;
   margin: 10px auto;
-  > .nav {
-    position: absolute;
-    top: 0;
-    right: 0;
-    > .anchor {
-      + .anchor {
-        margin-left: 10px;
+  > .link {
+    > .page {
+      position: absolute;
+      top: 0;
+      right: 0;
+      > .anchor {
+        + .anchor {
+          margin-left: 10px;
+        }
+      }
+    }
+    > .external {
+      position: absolute;
+      bottom: 0;
+      right: 0;
+      > .anchor {
+        + .anchor {
+          margin-left: 5px;
+        }
       }
     }
   }
-  > .logo {
-    width: 100px;
-    height: 100px;
-    display: block;
-    margin-right: 20px;
-  }
   > .information {
+    // height: 100px;
     display: flex;
     justify-content: flex-start;
     align-items: baseline;
-    > .arakicho {
+    > .logo {
+      width: 100px;
+      height: 100px;
+      display: block;
     }
-    > .name {
-      margin-left: 10px;
-      font-size: 24px;
-      font-weight: bold;
-    }
-    > .address {
-      margin-left: 30px;
-    }
-    > .tel {
-      margin-left: 10px;
+    > .text {
+      display: flex;
+      justify-content: flex-start;
+      align-items: baseline;
+      margin-left: 20px;
+      > .arakicho {
+      }
+      > .name {
+        margin-left: 10px;
+        font-size: 2.4rem;
+      }
+      > .address {
+        margin-left: 30px;
+      }
+      > .tel {
+        margin-left: 10px;
+      }
     }
   }
 }
 @media (max-width: 767px) {
   .l-footer {
-    height: calc(100vh / 7 * 2);
-    padding: {
-      left: 10px;
-      right: 10px;
-      bottom: 10px;
-    }
+    height: auto;
   }
   .c-footer {
-    height: 100%;
-    align-items: flex-end;
+    // height: calc(100% - 20px);
+    // flex-direction: column;
+    // align-items: stretch;
+    height: auto;
+    display: block;
+    margin: {
+      top: 5px;
+      bottom: 5px;
+    }
     padding: {
-      top: 10px;
+      left: 5px;
+      right: 5px;
     }
-    > .nav {
-      > .anchor {
-        + .anchor {
-        }
+    // align-items: flex-end;
+    // padding: {
+    //   top: 10px;
+    // }
+    > .link {
+      > .page {
+        // flex-basis: 100%;
+        position: relative;
+        margin-bottom: 10px;
+        text-align: center;
       }
-    }
-    > .logo {
+      > .external {
+        // flex-basis: 100%;
+        position: relative;
+        margin-bottom: 20px;
+        text-align: center;
+      }
     }
     > .information {
-      display: flex;
-      flex-direction: column;
-      > .arakicho {
+      align-items: flex-end;
+      > .logo {
+        width: 60px;
+        height: 60px;
       }
-      > .name,
-      > .address,
-      > .tel {
-        margin-left: 0;
+      > .text {
+        // display: flex;
+        flex-direction: column;
+        margin-left: 10px;
+        > .arakicho,
+        > .address,
+        > .tel {
+          font-size: 1.0rem;
+        }
+        > .name,
+        > .address,
+        > .tel {
+          margin-left: 0;
+        }
+        > .name {
+          font-size: 1.4rem;
+        }
       }
     }
   }
